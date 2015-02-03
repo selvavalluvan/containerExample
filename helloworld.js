15a73f85b053ae9778093e145d51a048171233e9
selvavalluvan <v.selvam.v@gmail.com>
2015-02-03 00:23:45 +0000

    awesome changes

--- helloworld.js
+++ helloworld.js
@@ -2,14 +2,14 @@ var express = require('express')
 var app = express()
 var argv = require('optimist').argv;
 
-app.get('/:id?', function (req, res) {
+app.get('/:id', function (req, res) {
 
-      if (!req.params.id) {
-            res.send("Hello World !");
-      } else {
-            res.send("Hello "+req.params.id+" !");
-      }
-})
+        res.send("Hello "+req.params.id+" !");
+});
 
-app.listen(3000, argv.fe_ip);
+app.get('/', function(req, res) {
+                res.send("hello World"); 
+        });
+        // listen (start app with node server.js) ======================================
+app.listen(8080, argv.fe_ip);
 console.log("App listening on port 8080");
